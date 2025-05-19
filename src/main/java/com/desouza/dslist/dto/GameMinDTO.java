aba68fd942ae5f490fece61a838ff62d8f544578
ozysouza <ozysouzaa@gmail.com>
2025-05-19 16:40:54 -0400

    Source Format

--- src/main/java/com/desouza/dslist/dto/GameMinDTO.java
+++ src/main/java/com/desouza/dslist/dto/GameMinDTO.java
@@ -41,5 +41,4 @@ public class GameMinDTO {
         return shortDescription;
     }
 
-    
 }
--- src/main/java/com/desouza/dslist/repositories/GameRepository.java
+++ src/main/java/com/desouza/dslist/repositories/GameRepository.java
@@ -3,6 +3,6 @@ package com.desouza.dslist.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.desouza.dslist.entities.Game;;
 
-public interface GameRepository extends JpaRepository<Game, Long>{
+public interface GameRepository extends JpaRepository<Game, Long> {
 
 }
