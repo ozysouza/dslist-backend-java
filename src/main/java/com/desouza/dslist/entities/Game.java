e8eef0758a3d18941bbd0aebef35bef29072b970
ozysouza <ozysouzaa@gmail.com>
2025-05-19 15:31:55 -0400

    Add column definition for long and short description

--- src/main/java/com/desouza/dslist/entities/Game.java
+++ src/main/java/com/desouza/dslist/entities/Game.java
@@ -24,7 +24,11 @@ public class Game {
     private String genre;
     private String platforms;
     private String imageUrl;
+
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game() {
