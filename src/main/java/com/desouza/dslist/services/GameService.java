ea3cddd6c501abaaad0229a24baa09938c7e1f15
ozysouza <ozysouzaa@gmail.com>
2025-05-20 16:02:12 -0400

    Create endpoint to get game by ID

--- src/main/java/com/desouza/dslist/services/GameService.java
+++ src/main/java/com/desouza/dslist/services/GameService.java
@@ -4,10 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.desouza.dslist.dto.GameDTO;
 import com.desouza.dslist.dto.GameMinDTO;
 import com.desouza.dslist.entities.Game;
 import com.desouza.dslist.repositories.GameRepository;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class GameService {
@@ -15,6 +19,14 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        Game result = gameRepository.findById(id)
+            .orElseThrow(() -> new EntityNotFoundException("Game with ID " + id + " not found"));
+        return new GameDTO(result);
+    }
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
         return result.stream().map(x -> new GameMinDTO(x)).toList();
