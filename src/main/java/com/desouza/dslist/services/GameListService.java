ebebdbdc427fe73bfa229515f1880b275d60087e
ozysouza <ozysouzaa@gmail.com>
2025-05-20 17:15:27 -0400

    Create endpoint to GET lists

---
+++ src/main/java/com/desouza/dslist/services/GameListService.java
@@ -0,0 +1,23 @@
+package com.desouza.dslist.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.desouza.dslist.dto.GameListDTO;
+import com.desouza.dslist.repositories.GameListRepository;
+
+@Service
+public class GameListService {
+
+    @Autowired
+    private GameListRepository gameListRepository;
+
+    @Transactional(readOnly = true)
+    public List<GameListDTO> findAll() {
+        return gameListRepository.findAll().stream().map(x -> new GameListDTO(x)).toList();
+    }
+
+}
